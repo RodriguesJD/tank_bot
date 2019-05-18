8bdafe92b50f3ba7d7737ecf2edcda2fdf2d9295
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-05-18 15:43:15 -0700

    Created file to setup systemd. This allows tank_bot to run at boot.

---
+++ tank_bot/tank_bot/systemd_files/system_d_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# this moves file into systemd.
+sudo cp tank_bot/tank_bot/systemd_files/tank_bot.service /etc/systemd/system/tank_bot.service
+
+# this enables tank_bot to run at boot.
+sudo systemctl enable tank_bot.service
