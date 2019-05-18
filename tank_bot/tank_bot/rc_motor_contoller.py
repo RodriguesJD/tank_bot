5b5aca555a49b435188760a37f25f0bc9652fe96
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-05-18 14:26:55 -0700

    Updated script from https://www.explainingcomputers.com/pi_devastator_videos.html.

--- tank_bot/tank_bot/rc_motor_contoller.py
+++ tank_bot/tank_bot/rc_motor_contoller.py
@@ -1,7 +1,8 @@
-# import curses, GPIO and time
+# import curses and GPIO
 import curses
 import RPi.GPIO as GPIO
-import time
+import os  # added so we can shut down OK
+import time  # import time module
 
 # set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BOARD)
@@ -9,6 +10,13 @@ GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
+GPIO.setup(29, GPIO.OUT)
+
+for x in range(1, 10):
+    GPIO.output(29, False)
+    time.sleep(.5)
+    GPIO.output(29, True)
+    time.sleep(1)
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
@@ -22,6 +30,8 @@ try:
         char = screen.getch()
         if char == ord('q'):
             break
+        if char == ord('S'):  # Added for shutdown on capital S
+            os.system('sudo shutdown now')  # shutdown right now!
         elif char == curses.KEY_UP:
             GPIO.output(7, False)
             GPIO.output(11, True)
@@ -42,27 +52,6 @@ try:
             GPIO.output(11, True)
             GPIO.output(13, True)
             GPIO.output(15, False)
-        elif char == ord('d'):
-            GPIO.output(11, True)
-            GPIO.output(15, True)
-            time.sleep(.5)
-            GPIO.output(7, True)
-            GPIO.output(11, False)
-            GPIO.output(13, True)
-            GPIO.output(15, False)
-            time.sleep(.5)
-            GPIO.output(7, True)
-            GPIO.output(11, False)
-            GPIO.output(13, False)
-            GPIO.output(15, True)
-            time.sleep(.5)
-            GPIO.output(7, False)
-            GPIO.output(11, True)
-            GPIO.output(13, True)
-            GPIO.output(15, False)
-            time.sleep(.5)
-            GPIO.output(11, False)
-            GPIO.output(13, False)
         elif char == 10:
             GPIO.output(7, False)
             GPIO.output(11, False)
@@ -71,8 +60,8 @@ try:
 
 finally:
     # Close down curses properly, inc turn echo back on!
-    curses.nocbreak();
-    screen.keypad(0);
+    curses.nocbreak()
+    screen.keypad(0)
     curses.echo()
     curses.endwin()
-    GPIO.cleanup(
+    GPIO.cleanup()
